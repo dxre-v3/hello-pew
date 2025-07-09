ed272b73da6afd268e60c53ea9de4074e0135edd
A. Allen <avriana.allen@gmail.com>
2025-07-09 10:44:32 -0700

    configure for repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hello/",
+  base: "/hello-pew/",
   server: {
     port: 5173,
   }
