78ccd1b36b566b326ea1dd7bf1e77274e33bdbb5
A. Allen <avriana.allen@gmail.com>
2025-07-09 20:06:37 -0700

    Push to live

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <>
-    <div>
+    <div> 
+      <h1 id="hello-world">Hello World!</h1>
       <p>"Coffee is a way of stealing time that should by rights belong to your older self."</p>
       <h4>― Terry Pratchett, Thud!</h4>
       <picture>
