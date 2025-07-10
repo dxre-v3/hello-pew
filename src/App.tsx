9d31f4d97151072852bce79ff3d6fee0d7e9a75a
A. Allen <avriana.allen@gmail.com>
2025-07-09 20:02:10 -0700

    Failure by design: removed 'Hello World!'

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ function App() {
 
   return (
     <>
-    <div> 
-      <h1 id="hello-world">Hello World!</h1>
+    <div>
+      <p>"Coffee is a way of stealing time that should by rights belong to your older self."</p>
+      <h4>― Terry Pratchett, Thud!</h4>
       <picture>
         <img src="./globe-a.webp" alt="🌎" width="40%" height="auto"></img>
       </picture>
